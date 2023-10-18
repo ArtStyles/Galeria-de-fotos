9967e3a55de31a39ec1d526f955641cbf3e3560d
ArtStyles <110346939+ArtStyles@users.noreply.github.com>
2023-10-18 12:53:47 -0400

    Adding modal and Fixing imge selection

--- actions.js
+++ actions.js
@@ -4,14 +4,20 @@ document.addEventListener('DOMContentLoaded',function(event){
        
     
     var picture = document.querySelectorAll(".gallery_picture");
-
+    var modal = document.getElementById("modal")
 
 picture.forEach((element,index) => {
     element.addEventListener('click', ()=>{
         if (element.classList.contains("activada")) {
             element.classList.remove("activada");
+            if (modal.classList.contains("modalSelected")) 
+                modal.classList.remove("modalSelected");
+
           } else {
             element.classList.add("activada");
+            modal.classList.add("modalSelected");
+    
+          
           }
     });
 });
